b294891028e85b1ba06747b6117dbd9ae137a673
shiv18prime-hub <shiv18prime@gmail.com>
2026-09-16 20:16:21 +0530

    Add DocumentFile support for PDF image export

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
     implementation("com.google.zxing:core:3.5.3")
 }
