69711426aa82683b0a6946376445aa66876a1e46
shiv18prime-hub <shiv18prime@gmail.com>
2026-09-16 19:42:26 +0530

    Add PDFBox engine for working PDF toolkit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,4 +34,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.tom-roush:pdfbox-android:2.0.27.0")
 }
