d84bc36db53ac17b98a1fff7fdcb7942216edff9
shiv18prime-hub <shiv18prime@gmail.com>
2026-09-16 17:04:12 +0530

    Add Google sign-in dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
